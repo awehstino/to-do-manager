abc65ad82d31b44d39ba9fd43728ca192213cd3c
blackfugee <Augusthne123@gmail.com>
2023-01-21 06:35:01 -0800

    save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import AddTaskform from "./components/AddTaskform";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import Message from "./components/Message";
 import { useState } from "react";
 const App = () => {
   const [showaddTask, setshowaddTask] = useState(false)
-  const [tasks, setTasks] = useState(["ayomide codm player","liminydo codm player","Gun smoke codm player","callme dammy codm player","dolipee codm player"])
-  const addtask = (task)=>{
-    
-    
-    setTasks([...tasks, task])
+const [tasks, setTasks] = useState([{id :1, text:"ayomide codm player"}, {id :2, text:"liminydo codm player"},{id :3, text:"Gun smoke codm player"}, {id :4, text:"callme dammy codm player"}, {id :5, text:"dolipee codm player"}])
+  const addtask = (text)=>{
+    const id = Math.floor(Math.random()* 10000)+1
+    const newtask = { id, ...text}
+    setTasks([...tasks, newtask])
+    console.log(newtask)
   }
   //for deleting task
   const deleteTask = (id) =>{
@@ -20,7 +22,7 @@ const App = () => {
       <Header onadd={()=> setshowaddTask (!showaddTask) } />
       {showaddTask && <AddTaskform   onadd={addtask}/>}
       <hr />
-      <Tasks tasks={tasks} />
+      { tasks.length > 0 ? <Tasks tasks={tasks}  ondelete={deleteTask}/> : <Message />}
     </div>
   )  
 }
